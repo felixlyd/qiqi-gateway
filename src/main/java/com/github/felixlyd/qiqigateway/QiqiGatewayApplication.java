c1604ab83e5c7a7321e8a9c5b6ba6427d15cb313
YaodongLiu <ttttttliu@qq.com>
2023-05-05 14:15:06 +0800

    Initial commit

---
+++ src/main/java/com/github/felixlyd/qiqigateway/QiqiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.felixlyd.qiqigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiqiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QiqiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/felixlyd/qiqigateway/QiqiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felixlyd.qiqigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QiqiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
