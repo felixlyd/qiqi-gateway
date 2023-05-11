835052710107c3defb454401f4b95d7726bc7227
YaodongLiu <ttttttliu@qq.com>
2023-05-11 10:56:09 +0800

    初始化网关app

---
+++ qiqi-app/src/main/java/com/github/felixlyd/QiqiApplication.java
@@ -0,0 +1,17 @@
+package com.github.felixlyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * class QiqiApplication: do something
+ *
+ * @author : liuyaodong
+ * @date 2023/5/9
+ */
+@SpringBootApplication
+public class QiqiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QiqiApplication.class,args);
+    }
+}
