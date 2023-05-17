b37f7a16127fde3f304915d6954949281f9ed4ef
YaodongLiu <ttttttliu@qq.com>
2023-05-17 16:23:58 +0800

    spring-boot-admin使用sa-token控制登录commit记录留存代码 master分支默认sa-token，另有spring-security-metric分支保留spring-security代码

--- qiqi-graphic/qiqi-metric/src/main/java/com/github/felixlyd/config/SaTokenSecureConfig.java
+++ qiqi-graphic/qiqi-metric/src/main/java/com/github/felixlyd/config/SaTokenSecureConfig.java
@@ -1,5 +1,9 @@
 package com.github.felixlyd.config;
 
+import com.github.felixlyd.config.properties.SecureLoginProperties;
+import com.github.felixlyd.config.properties.SecureProperties;
+import com.github.felixlyd.filter.LoginFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,15 +15,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SaTokenSecureConfig {
 
-//    /**
-//     * 方案1：sa-token
-//     * 引入spring security后，该代码需要注释
-//     * @param saTokenProperties saToken配置
-//     * @param saTokenLoginProperties saToken登录配置
-//     * @return 登录过滤器
-//     */
-//    @Bean
-//    public LoginFilter loginFilter(SecureProperties saTokenProperties, SecureLoginProperties saTokenLoginProperties){
-//        return new LoginFilter(saTokenProperties, saTokenLoginProperties);
-//    }
+    /**
+     * 方案1：sa-token
+     * 引入spring security后，该代码需要注释
+     * @param secureProperties saToken配置
+     * @param secureLoginProperties saToken登录配置
+     * @return 登录过滤器
+     */
+    @Bean
+    public LoginFilter loginFilter(SecureProperties secureProperties, SecureLoginProperties secureLoginProperties){
+        return new LoginFilter(secureProperties, secureLoginProperties);
+    }
 }
--- qiqi-graphic/qiqi-metric/src/main/java/com/github/felixlyd/config/SecuritySecureConfig.java
+++ qiqi-graphic/qiqi-metric/src/main/java/com/github/felixlyd/config/SecuritySecureConfig.java
@@ -1,71 +1,71 @@
-package com.github.felixlyd.config;
-
-import cn.hutool.core.lang.UUID;
-import com.github.felixlyd.config.properties.SecureLoginProperties;
-import com.github.felixlyd.config.properties.SecureProperties;
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-/**
- * class SecuritySecureConfig: do something
- *
- * @author : liuyaodong
- * @date 2023/5/17
- */
-@Configuration(proxyBeanMethods = false)
-@EnableWebSecurity
-public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
-    private final AdminServerProperties adminServer;
-    private final SecureProperties secureProperties;
-    private final SecureLoginProperties secureLoginProperties;
-
-    public SecuritySecureConfig(AdminServerProperties adminServer, SecureProperties secureProperties, SecureLoginProperties secureLoginProperties) {
-        this.adminServer = adminServer;
-        this.secureProperties = secureProperties;
-        this.secureLoginProperties = secureLoginProperties;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        successHandler.setTargetUrlParameter("redirectTo");
-        successHandler.setDefaultTargetUrl(secureLoginProperties.getIndexUrl());
-
-        http.authorizeRequests(
-                        (authorizeRequests) -> authorizeRequests.antMatchers(secureProperties.getExcludeList().get(0)).permitAll()
-                                .antMatchers(secureProperties.getExcludeList().get(1)).permitAll()
-                                .antMatchers(secureProperties.getExcludeList().get(2)).permitAll()
-                                .antMatchers(secureProperties.getExcludeList().get(3)).permitAll()
-                                .antMatchers(secureProperties.getExcludeList().get(4)).permitAll().anyRequest().authenticated()
-                ).formLogin(
-                        (formLogin) -> formLogin.loginPage(secureLoginProperties.getLoginUrl()).successHandler(successHandler).and()
-                ).logout((logout) -> logout.logoutUrl(secureLoginProperties.getLogoutUrl())).httpBasic(Customizer.withDefaults())
-                .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .ignoringRequestMatchers(
-                                new AntPathRequestMatcher(this.adminServer.path("/instances"),
-                                        HttpMethod.POST.toString()),
-                                new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
-                                        HttpMethod.DELETE.toString()),
-                                new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
-                        ))
-                .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600));
-    }
-
-    // Required to provide UserDetailsService for "remember functionality"
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(secureLoginProperties.getUsername())
-                .password("{noop}" + secureLoginProperties.getPassword()).roles("USER");
-    }
-
-
-}
+//package com.github.felixlyd.config;
+//
+//import cn.hutool.core.lang.UUID;
+//import com.github.felixlyd.config.properties.SecureLoginProperties;
+//import com.github.felixlyd.config.properties.SecureProperties;
+//import de.codecentric.boot.admin.server.config.AdminServerProperties;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+///**
+// * class SecuritySecureConfig: do something
+// *
+// * @author : liuyaodong
+// * @date 2023/5/17
+// */
+//@Configuration(proxyBeanMethods = false)
+//@EnableWebSecurity
+//public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+//    private final AdminServerProperties adminServer;
+//    private final SecureProperties secureProperties;
+//    private final SecureLoginProperties secureLoginProperties;
+//
+//    public SecuritySecureConfig(AdminServerProperties adminServer, SecureProperties secureProperties, SecureLoginProperties secureLoginProperties) {
+//        this.adminServer = adminServer;
+//        this.secureProperties = secureProperties;
+//        this.secureLoginProperties = secureLoginProperties;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+//        successHandler.setTargetUrlParameter("redirectTo");
+//        successHandler.setDefaultTargetUrl(secureLoginProperties.getIndexUrl());
+//
+//        http.authorizeRequests(
+//                        (authorizeRequests) -> authorizeRequests.antMatchers(secureProperties.getExcludeList().get(0)).permitAll()
+//                                .antMatchers(secureProperties.getExcludeList().get(1)).permitAll()
+//                                .antMatchers(secureProperties.getExcludeList().get(2)).permitAll()
+//                                .antMatchers(secureProperties.getExcludeList().get(3)).permitAll()
+//                                .antMatchers(secureProperties.getExcludeList().get(4)).permitAll().anyRequest().authenticated()
+//                ).formLogin(
+//                        (formLogin) -> formLogin.loginPage(secureLoginProperties.getLoginUrl()).successHandler(successHandler).and()
+//                ).logout((logout) -> logout.logoutUrl(secureLoginProperties.getLogoutUrl())).httpBasic(Customizer.withDefaults())
+//                .csrf((csrf) -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                        .ignoringRequestMatchers(
+//                                new AntPathRequestMatcher(this.adminServer.path("/instances"),
+//                                        HttpMethod.POST.toString()),
+//                                new AntPathRequestMatcher(this.adminServer.path("/instances/*"),
+//                                        HttpMethod.DELETE.toString()),
+//                                new AntPathRequestMatcher(this.adminServer.path("/actuator/**"))
+//                        ))
+//                .rememberMe((rememberMe) -> rememberMe.key(UUID.randomUUID().toString()).tokenValiditySeconds(1209600));
+//    }
+//
+//    // Required to provide UserDetailsService for "remember functionality"
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser(secureLoginProperties.getUsername())
+//                .password("{noop}" + secureLoginProperties.getPassword()).roles("USER");
+//    }
+//
+//
+//}
